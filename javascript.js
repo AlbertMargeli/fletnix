9150eb8326e5a586e09231d2ce45167ec110900d
AlbertMargeli <albertmargelibailo@gmail.com>
2022-05-23 11:28:35 +0200

    fletnix GO!

---
+++ javascript.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){ /* inici JQUERY */
+
+/* recopila JSON */
+
+$.getJSON ("llista_original.php", function(pelis) {
+
+    setTimeout(function() {$("#carregant").fadeOut();}, 100)
+
+
+    estrellaOn = '<img src="img/stars/star_on.png">';
+    estrellaHalf = '<img src="img/stars/star_half.png">';
+    estrellaOff = '<img src="img/stars/star_off.png">';
+
+    /* main loop */
+    
+    for (n=0; n<pelis.length; n++) { 
+
+        /* calcular estrellas */
+
+        puntuacioEntero = parseInt((pelis[n]["puntuacio"])/2);
+        puntuacioResto = (pelis[n]["puntuacio"])%2;
+        menosQueCinco = 5-puntuacioEntero;
+        mostrarEstrellas = "";
+
+        if(puntuacioResto==0) {
+            for(z=0; z<puntuacioEntero; z++) {
+                mostrarEstrellas = mostrarEstrellas + estrellaOn;}
+            for(y=0; y<menosQueCinco; y++) {mostrarEstrellas = mostrarEstrellas+estrellaOff;}}
+            
+            else {
+                for(z=0; z<puntuacioEntero; z++) {
+                    mostrarEstrellas = mostrarEstrellas + estrellaOn;}
+                mostrarEstrellas = mostrarEstrellas + estrellaHalf;
+                for(y=1; y<menosQueCinco; y++) {mostrarEstrellas = mostrarEstrellas+estrellaOff;}}
+
+        imprimir = '<li class="splide__slide movie"><div class="movie-back"><h2 class="titol">'+pelis[n]["titol"]+'</h2><img class="portada" src="img/covers/'+pelis[n]["image"]+'.jpg"><h3 class="director">'+pelis[n]["director"]+'</h3><p class="dades"><span class="any">'+pelis[n]["any"]+'</span> / <span class="pais">'+pelis[n]["pais"]+'</span></p><a class="google" href="https://www.google.com/search?q='+pelis[n]["titol"]+' '+pelis[n]["director"]+' " target="_blank">Més info</a><div class="stars">'+mostrarEstrellas+'</div><div></li>'
+
+        if(pelis[n]["any"]>1999) {$('#splide__list_sxxi').append(imprimir);}
+            else if(pelis[n]["any"]<1980) {$('#splide__list_classic').append(imprimir);}       
+                else {$('#splide__list_fin-milenio').append(imprimir);}
+
+        
+        if(pelis[n]["dona"]==1) {$('#splide__list_she').append(imprimir);}
+        
+        if(pelis[n]["comedia"]==1) {$('#splide__list_comedia').append(imprimir);}
+
+        if(pelis[n]["pais"]!="USA" && pelis[n]["pais"]!="UK") {$('#splide__list_no-english').append(imprimir);}
+
+
+        /* layout pel·lícules */
+
+        
+
+        
+    }
+
+    var elms = document.getElementsByClassName( 'splide' );
+    for ( var i = 0; i < elms.length; i++ ) {
+    new Splide( elms[ i ], {type: 'loop', perPage: 6}).mount();
+}
+
+
+/* final JSON */
+});
+
+
+
+
+
+/* final JQUERY */ });
+
+
+
