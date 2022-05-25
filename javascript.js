e5ddd8d88945d26b46ad2e48bf4dcc93529029dd
AlbertMargeli <albertmargelibailo@gmail.com>
2022-05-25 07:49:36 +0200

    començo slider gran

--- javascript.js
+++ javascript.js
@@ -125,10 +125,88 @@ $("#link-no-english").click(function()
 
 /* MOBILE MENU */
 
-$("#mobile-menu").click(function() {
-    $("#off-canvas").animate({top: "200px"});
+function ensenyarCanvas() {
+    $("#off-canvas").css("display" , "flex")
+    $("#off-canvas").addClass("topzero")
     $("body").css("overflow", "hidden")
+    $("#mobile-menu").attr("name" , "esconder-canvas")
+    $(".barra-burger").addClass("estrenyer")}
+
+function amagarCanvas() {
+    $("#off-canvas").fadeOut(1200);
+    $("body").css("overflow", "auto")
+    $("#mobile-menu").attr("name" , "mostrar-canvas")
+    setTimeout(function() {$("#off-canvas").removeClass("topzero");}, 1300)}
+
+$("#mobile-menu").click(function() {
+    var z = $(this).attr("name");
+    if (z=="mostrar-canvas") {
+        ensenyarCanvas();}
+    else if (z=="esconder-canvas"){
+        amagarCanvas();}
 });
+
+$("#link-sxxi-off").click(function()
+{   
+    $("section").removeClass("order01");
+    $("section").addClass("order02");
+    $("#sxxi").removeClass("order02");
+    $("#sxxi").addClass("order01");
+    window.scrollTo(0, 0);
+    amagarCanvas();
+});
+
+$("#link-fin-milenio-off").click(function()
+{   
+    $("section").removeClass("order01");
+    $("section").addClass("order02");
+    $("#fin-milenio").removeClass("order02");
+    $("#fin-milenio").addClass("order01");
+    window.scrollTo(0, 0);
+    amagarCanvas();
+});
+
+$("#link-classic-off").click(function()
+{   
+    $("section").removeClass("order01");
+    $("section").addClass("order02");
+    $("#classic").removeClass("order02");
+    $("#classic").addClass("order01");
+    window.scrollTo(0, 0);
+    amagarCanvas();
+});
+
+$("#link-she-off").click(function()
+{   
+    $("section").removeClass("order01");
+    $("section").addClass("order02");
+    $("#she").removeClass("order02");
+    $("#she").addClass("order01");
+    window.scrollTo(0, 0);
+    amagarCanvas();
+});
+
+$("#link-comedia-off").click(function()
+{   
+    $("section").removeClass("order01");
+    $("section").addClass("order02");
+    $("#comedia").removeClass("order02");
+    $("#comedia").addClass("order01");
+    window.scrollTo(0, 0);
+    amagarCanvas();
+});
+
+$("#link-no-english-off").click(function()
+{   
+    $("section").removeClass("order01");
+    $("section").addClass("order02");
+    $("#no-english").removeClass("order02");
+    $("#no-english").addClass("order01");
+    window.scrollTo(0, 0);
+    amagarCanvas();
+    // 
+});
+
 
 
 
