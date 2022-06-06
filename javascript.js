4bf22aa84382b52015d182c01c1fb5b4361a567c
AlbertMargeli <albertmargelibailo@gmail.com>
2022-06-06 13:21:28 +0200

    entrega final

--- javascript.js
+++ javascript.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){ /* inici JQUERY */
 
+setTimeout(function() {$("#carregant").fadeOut();}, 3000);
+
+// window.onload = function() {
+//     $("#carregant").fadeOut()};
+
 /* recopila JSON */
 
 $.getJSON ("llista_original.php", function(pelis) {
-
-    /*setTimeout(function() {$("#carregant").fadeOut();}, 4000)*/
-
 
     estrellaOn = '<img src="img/stars/star_on.png">';
     estrellaHalf = '<img src="img/stars/star_half.png">';
@@ -15,7 +17,14 @@ $.getJSON ("llista_original.php", function(pelis) {
     
     for (n=0; n<pelis.length; n++) { 
 
-        /* calcular estrellas */
+        /* llargada titol / calcular estrellas */
+
+        longitudTitol = pelis[n]["titol"].length;
+        mostrarTitol = "";
+        if(longitudTitol>30) {
+            mostrarTitol = '<h2 class="titol titol-llarg">'+pelis[n]["titol"]+'</h2>';}
+            else {mostrarTitol = '<h2 class="titol">'+pelis[n]["titol"]+'</h2>';}
+
 
         puntuacioEntero = parseInt((pelis[n]["puntuacio"])/2);
         puntuacioResto = (pelis[n]["puntuacio"])%2;
@@ -35,7 +44,7 @@ $.getJSON ("llista_original.php", function(pelis) {
 
         /* layout pel·lícules */   
 
-        imprimir = '<li class="splide__slide movie"><div class="movie-back"><h2 class="titol">'+pelis[n]["titol"]+'</h2><img class="portada" src="img/covers/'+pelis[n]["image"]+'.jpg"><h3 class="director">'+pelis[n]["director"]+'</h3><p class="dades"><span class="any">'+pelis[n]["any"]+'</span> / <span class="pais">'+pelis[n]["pais"]+'</span></p><a class="google" href="https://www.google.com/search?q='+pelis[n]["titol"]+' '+pelis[n]["director"]+' " target="_blank">Més info</a><div class="stars">'+mostrarEstrellas+'</div><a href="eliminar_pellicula.php?id='+pelis[n]["id"]+'" class="eliminar">Elimina pel·lícula</a><div></li>'
+        imprimir = '<li class="splide__slide movie"><div class="movie-back">'+mostrarTitol+'</h2><img class="portada" src="img/covers/'+pelis[n]["image"]+'.jpg"><h3 class="director">'+pelis[n]["director"]+'</h3><p class="dades"><span class="any">'+pelis[n]["any"]+'</span> / <span class="pais">'+pelis[n]["pais"]+'</span></p><a class="google" href="https://www.google.com/search?q='+pelis[n]["titol"]+' '+pelis[n]["director"]+' " target="_blank">Més info</a><div class="stars">'+mostrarEstrellas+'</div><a href="eliminar_pellicula.php?id='+pelis[n]["id"]+'" class="eliminar">Elimina pel·lícula</a><div></li>'
 
         if(pelis[n]["any"]>1999) {$('#splide__list_sxxi').append(imprimir);}
             else if(pelis[n]["any"]<1980) {$('#splide__list_classic').append(imprimir);}       
@@ -58,15 +67,18 @@ $.getJSON ("llista_original.php", function(pelis) {
         
         var elms = document.getElementsByClassName( 'splide' );
         for ( var i = 0; i < elms.length; i++ ) {
-
-            if(i==0) {new Splide( elms[ i ], {type: 'loop', perPage: 1, pagination: false, autoplay: true, interval: 5000}).mount();}
+    
+            if(i==0) {new Splide( elms[ i ], {type: 'loop', perPage: 1, pagination: false, /* autoplay: true chocho ,*/ interval: 5000}).mount();}
             else {new Splide( elms[ i ], {type: 'loop', perPage: 6}).mount();}}}
         
-    else {
+        else {
+            
+            var elms = document.getElementsByClassName( 'splide' );
 
-        var elms = document.getElementsByClassName( 'splide' );
-        for ( var i = 0; i < elms.length; i++ ) {
-        new Splide( elms[ i ], {type: 'loop', perPage: 2, arrows: false}).mount();}};
+            for ( var i = 0; i < elms.length; i++ ) {
+    
+                if(i==0) {new Splide( elms[ i ], {type: 'loop', perPage: 1, pagination: false, arrows: false, /* autoplay: true chocho ,*/ interval: 5000}).mount();}
+                else {new Splide( elms[ i ], {type: 'loop', perPage: 2, arrows: false}).mount();}}}
 
 
 /* final JSON */
@@ -115,7 +127,7 @@ $("#link-sxxi").click(function()
     $(".main-container").addClass("order02");
     $("#sxxi").removeClass("order02");
     $("#sxxi").addClass("order01");
-    window.scrollTo(0, 671);
+    window.scrollTo(0, 680);
 });
 
 $("#link-fin-milenio").click(function()
@@ -124,7 +136,7 @@ $("#link-fin-milenio").click(function()
     $(".main-container").addClass("order02");
     $("#fin-milenio").removeClass("order02");
     $("#fin-milenio").addClass("order01");
-    window.scrollTo(0, 671);
+    window.scrollTo(0, 680);
 });
 
 $("#link-classic").click(function()
@@ -133,7 +145,7 @@ $("#link-classic").click(function()
     $(".main-container").addClass("order02");
     $("#classic").removeClass("order02");
     $("#classic").addClass("order01");
-    window.scrollTo(0, 671);
+    window.scrollTo(0, 680);
 });
 
 $("#link-she").click(function()
@@ -142,7 +154,7 @@ $("#link-she").click(function()
     $(".main-container").addClass("order02");
     $("#she").removeClass("order02");
     $("#she").addClass("order01");
-    window.scrollTo(0, 671);
+    window.scrollTo(0, 680);
 });
 
 $("#link-comedia").click(function()
@@ -151,7 +163,7 @@ $("#link-comedia").click(function()
     $(".main-container").addClass("order02");
     $("#comedia").removeClass("order02");
     $("#comedia").addClass("order01");
-    window.scrollTo(0, 671);
+    window.scrollTo(0, 680);
 });
 
 $("#link-no-english").click(function()
@@ -160,143 +172,211 @@ $("#link-no-english").click(function()
     $(".main-container").addClass("order02");
     $("#no-english").removeClass("order02");
     $("#no-english").addClass("order01");
-    window.scrollTo(0, 671);
+    window.scrollTo(0, 680);
 });
+
+/* SHRINKING */
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    $("#motto").addClass("shrink");
+    $("#logo").addClass("shrink");
+    $("#header-inner").addClass("shrink");
+    $("#logoh1").addClass("shrink");
+    $("#off-canvas-but").addClass("shrink");
+    $("#mobile-menu").addClass("shrink");}
+    
+else {
+    $("#motto").removeClass("shrink");
+    $("#logo").removeClass("shrink");
+    $("#header-inner").removeClass("shrink");
+    $("#logoh1").removeClass("shrink");
+    $("#off-canvas-but").removeClass("shrink");
+    $("#mobile-menu").removeClass("shrink");}
+}
 
 /* MOBILE MENU */
 
 function ensenyarCanvas() {
     $("#off-canvas").css("display" , "flex")
     $("#off-canvas").addClass("topzero")
-    $("body").css("overflow", "hidden")
-    $("#mobile-menu").attr("name" , "esconder-canvas")
-    $(".barra-burger").addClass("estrenyer")}
+    $("#burger-but").addClass("estrenyer")
+    $("#cross-but").addClass("eixamplar")
+    $("#obrir-off-canvas").removeClass("active")
+    $("#abrir-menu-label").fadeOut(400, function(){$("#cerrar-menu-label").fadeIn(1400)})
+    }
 
 function amagarCanvas() {
     $("#off-canvas").fadeOut(1200);
-    $("body").css("overflow", "auto")
-    $("#mobile-menu").attr("name" , "mostrar-canvas")
-    setTimeout(function() {$("#off-canvas").removeClass("topzero");}, 1300)}
+    $("#cross-but").removeClass("eixamplar")
+    $("#cross-but").addClass("estrenyer")
+    $("#burger-but").removeClass("estrenyer")
+    $("#burger-but").addClass("eixamplar")
+    $("#obrir-off-canvas").addClass("active")
+    $("#cerrar-menu-label").fadeOut(400, function(){$("#abrir-menu-label").fadeIn(1400)})
+    setTimeout(function() {$("#off-canvas").removeClass("topzero"); $("#burger-but").removeClass("eixamplar")}, 1300)}
 
-$("#mobile-menu").click(function() {
-    var z = $(this).attr("name");
-    if (z=="mostrar-canvas") {
-        ensenyarCanvas();}
-    else if (z=="esconder-canvas"){
-        amagarCanvas();}
-});
+$("#off-canvas-but").click(function() {
+    if($("#obrir-off-canvas").attr("class")=="active") {
+        ensenyarCanvas()}
+    else {amagarCanvas()}});
 
 $("#link-sxxi-off").click(function()
 {   
-    $("section").removeClass("order01");
-    $("section").addClass("order02");
+    $(".main-container").removeClass("order01");
+    $(".main-container").addClass("order02");
     $("#sxxi").removeClass("order02");
     $("#sxxi").addClass("order01");
-    window.scrollTo(0, 0);
+    $("body").scrollTop(725);
+    document.documentElement.scrollTop = 725;
     amagarCanvas();
 });
 
 $("#link-fin-milenio-off").click(function()
 {   
-    $("section").removeClass("order01");
-    $("section").addClass("order02");
+    $(".main-container").removeClass("order01");
+    $(".main-container").addClass("order02");
     $("#fin-milenio").removeClass("order02");
     $("#fin-milenio").addClass("order01");
-    window.scrollTo(0, 0);
+    $("body").scrollTop(725);
+    document.documentElement.scrollTop = 725;
     amagarCanvas();
 });
 
 $("#link-classic-off").click(function()
 {   
-    $("section").removeClass("order01");
-    $("section").addClass("order02");
+    $(".main-container").removeClass("order01");
+    $(".main-container").addClass("order02");
     $("#classic").removeClass("order02");
     $("#classic").addClass("order01");
-    window.scrollTo(0, 0);
+    $("body").scrollTop(725);
+    document.documentElement.scrollTop = 725;
     amagarCanvas();
 });
 
 $("#link-she-off").click(function()
 {   
-    $("section").removeClass("order01");
-    $("section").addClass("order02");
+    $(".main-container").removeClass("order01");
+    $(".main-container").addClass("order02");
     $("#she").removeClass("order02");
     $("#she").addClass("order01");
-    window.scrollTo(0, 0);
+    $("body").scrollTop(725);
+    document.documentElement.scrollTop = 725;
     amagarCanvas();
 });
 
 $("#link-comedia-off").click(function()
 {   
-    $("section").removeClass("order01");
-    $("section").addClass("order02");
+    $(".main-container").removeClass("order01");
+    $(".main-container").addClass("order02");
     $("#comedia").removeClass("order02");
     $("#comedia").addClass("order01");
-    window.scrollTo(0, 0);
+    $("body").scrollTop(725);
+    document.documentElement.scrollTop = 725;
     amagarCanvas();
 });
 
 $("#link-no-english-off").click(function()
 {   
-    $("section").removeClass("order01");
-    $("section").addClass("order02");
+    $(".main-container").removeClass("order01");
+    $(".main-container").addClass("order02");
     $("#no-english").removeClass("order02");
     $("#no-english").addClass("order01");
-    window.scrollTo(0, 0);
+    $("body").scrollTop(725);
+    document.documentElement.scrollTop = 725;
     amagarCanvas();
-    // 
 });
 
 /* TRAILERS */
 
 $("#button-trailer-shadows").click(function(){
-    $("#iframe-container-shadows").html('<iframe src="video-shadows.html" scrolling="no";></iframe>')
-    $("#iframe-shadows").addClass("moure-cap-a-dins");})
-    
+    $("#trailer-shadows").append('<video id="video-shadows" class="video-container" controls autoplay loop poster="img/shadows-portada.jpg"><source src="https://pruebaprimero.com/fletnix-videos/shadows-video.mp4" type="video/mp4"></video>')
+    $("#trailer-shadows ").addClass("moure-cap-a-dins");})
+
 $("#button-trailer-girl").click(function(){
-    $("#iframe-container-girl").html('<iframe src="video-girl.html" scrolling="no";></iframe>')
-    $("#iframe-girl").addClass("moure-cap-a-dins");})
+    $("#trailer-girl").append('<video id="video-girl" class="video-container" controls autoplay loop poster="img/girl-portada.jpg"><source src="https://pruebaprimero.com/fletnix-videos/girl-video.mp4" type="video/mp4"></video>')
+    $("#trailer-girl ").addClass("moure-cap-a-dins");})
 
 $("#button-trailer-drive").click(function(){
-    $("#iframe-container-drive").html('<iframe src="video-drive.html" scrolling="no";></iframe>')
-    $("#iframe-drive").addClass("moure-cap-a-dins");})
+    $("#trailer-drive").append('<video id="video-drive" class="video-container" controls autoplay loop poster="img/drive-portada.jpg"><source src="https://pruebaprimero.com/fletnix-videos/drive-video.mp4" type="video/mp4"></video>')
+    $("#trailer-drive ").addClass("moure-cap-a-dins");})
 
 $("#tancar-shadows").click(function(){
-    $("#iframe-shadows").removeClass("fora-pantalla");
-    $("#iframe-shadows").removeClass("moure-cap-a-dins")
-    $("#iframe-shadows").addClass("moure-cap-a-fora");
-    $("#iframe-container-shadows").html('')
+    $("#trailer-shadows").removeClass("fora-pantalla");
+    $("#trailer-shadows").removeClass("moure-cap-a-dins")
+    $("#trailer-shadows").addClass("moure-cap-a-fora");
     setTimeout(function() {
-        $("#iframe-shadows").removeClass("moure-cap-a-fora")
-        $("#iframe-shadows").addClass("fora-pantalla");}
-        , 1200)})
+        $("#video-shadows").remove();
+        $("#trailer-shadows").removeClass("moure-cap-a-fora")
+        $("#trailer-shadows").addClass("fora-pantalla");}
+        , 701)})
 
 $("#tancar-girl").click(function(){
-    $("#iframe-girl").removeClass("fora-pantalla");
-    $("#iframe-girl").removeClass("moure-cap-a-dins")
-    $("#iframe-girl").addClass("moure-cap-a-fora");
-    $("#iframe-container-girl").html('')
+    $("#trailer-girl").removeClass("fora-pantalla");
+    $("#trailer-girl").removeClass("moure-cap-a-dins")
+    $("#trailer-girl").addClass("moure-cap-a-fora");
     setTimeout(function() {
-        $("#iframe-girl").removeClass("moure-cap-a-fora")
-        $("#iframe-girl").addClass("fora-pantalla");}
-        , 1200)})
+        $("#video-girl").remove();
+        $("#trailer-girl").removeClass("moure-cap-a-fora")
+        $("#trailer-girl").addClass("fora-pantalla");}
+        , 701)})
 
 $("#tancar-drive").click(function(){
-    $("#iframe-drive").removeClass("fora-pantalla");
-    $("#iframe-drive").removeClass("moure-cap-a-dins")
-    $("#iframe-drive").addClass("moure-cap-a-fora");
-    $("#iframe-container-drive").html('');
+    $("#trailer-drive").removeClass("fora-pantalla");
+    $("#trailer-drive").removeClass("moure-cap-a-dins")
+    $("#trailer-drive").addClass("moure-cap-a-fora");
     setTimeout(function() {
-        $("#iframe-drive").removeClass("moure-cap-a-fora")
-        $("#iframe-drive").addClass("fora-pantalla");}
-        , 1200)});
+        $("#video-drive").remove();
+        $("#trailer-drive").removeClass("moure-cap-a-fora")
+        $("#trailer-drive").addClass("fora-pantalla");}
+        , 701)})
 
-$(".menu-button").click(function(){
-    $(".iframe-container-trailer").html('');
-    $(".iframe-container").removeClass("moure-cap-a-dins");
-    $(".iframe-container").addClass("fora-pantalla");});
+/* IMATGES MOBIL */
+
+var dispositivesResponsivePics = window.matchMedia('(max-device-width: 767px)');
+if(dispositivesResponsivePics.matches) {
+    $("#pic-fondo-shadows").attr("src", "img/shadows-portada.jpg");
+    $("#pic-fondo-girl").attr("src", "img/girl-portada.jpg");
+    $("#pic-fondo-drive").attr("src", "img/drive-portada.jpg");}
+
+/* SWIPE */
+
+ $("#shadows-shadows").onSwipe(function(results){
+     if(results.right == true) {
+    $("#trailer-shadows").removeClass("fora-pantalla");
+    $("#trailer-shadows").removeClass("moure-cap-a-dins")
+    $("#trailer-shadows").addClass("moure-cap-a-fora");
+    setTimeout(function() {
+        $("#video-shadows").remove();
+        $("#trailer-shadows").removeClass("moure-cap-a-fora")
+        $("#trailer-shadows").addClass("fora-pantalla");}
+        , 701)}});
+
+$("#girl-girl").onSwipe(function(results){
+    if(results.right == true) {
+    $("#trailer-girl").removeClass("fora-pantalla");
+    $("#trailer-girl").removeClass("moure-cap-a-dins")
+    $("#trailer-girl").addClass("moure-cap-a-fora");
+    setTimeout(function() {
+        $("#video-girl").remove();
+        $("#trailer-girl").removeClass("moure-cap-a-fora")
+        $("#trailer-girl").addClass("fora-pantalla");}
+        , 701)}});
+
+$("#drive-drive").onSwipe(function(results){
+    if(results.right == true) {
+    $("#trailer-drive").removeClass("fora-pantalla");
+    $("#trailer-drive").removeClass("moure-cap-a-dins")
+    $("#trailer-drive").addClass("moure-cap-a-fora");
+    setTimeout(function() {
+        $("#video-drive").remove();
+        $("#trailer-drive").removeClass("moure-cap-a-fora")
+        $("#trailer-drive").addClass("fora-pantalla");}
+        , 701)}});
+
+
 
 
 /* final JQUERY */ });
-
-
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-$(function(){
-    $('#banner-datos-contacto').each(function(){
-        $(this).insertBAfter($("#primary").parent())
-    })
-})
+
+        $("#motto").removeClass("shrink");
+        $("#logo").removeClass("shrink");
+        $("#header-inner").removeClass("shrink");
+        $("#mobile-menu-label").removeClass("shrink");
+        $("#logoh1").removeClass("shrink");
